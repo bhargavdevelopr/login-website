dc8ba8c220a9b0522ddf01956c2782c4d7ea28b2
bhargavdevelopr <154773980+bhargavdevelopr@users.noreply.github.com>
2024-03-05 08:38:38 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import './index.css'
 const Header = props => {
   const onClickLogout = () => {
     const {history} = props
-    Cookies.remove('jwt_Token')
+    Cookies.remove('jwt_token')
     history.replace('/login')
   }
   return (
